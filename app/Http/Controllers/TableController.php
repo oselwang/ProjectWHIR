b5c5064cc1110ee20622d5ae3998b3efb4ca4e76
vagrant <osel.wang@yahoo.com>
2016-05-15 17:56:36 +0000

    Lot

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -12,4 +12,51 @@ class TableController extends Controller
     protected $variable;
     protected $user;
 
+    public function __construct(Variable $variable, User $user)
+    {
+        $this->user = $user;
+        $this->variable = $variable;
+        $this->middleware('admin');
+    }
+
+    public function doctor()
+    {
+
+    }
+
+    public function form()
+    {
+        $variables = $this->variable->paginate(10);
+
+        $users = $this->user->get();
+
+        $completed = count($this->variable->where('status', 'completed')->get());
+        $uncompleted = count($this->variable->where('status', 'uncompleted')->get());
+
+        return view('formtable', compact('variables', 'completed', 'uncompleted', 'users'));
+    }
+
+
+    public function exportExcel()
+    {
+
+        Excel::create('Whir Form', function ($excel) {
+
+            $excel->sheet('New sheet', function ($sheet) {
+                $variables = $this->variable->get();
+                $sheet->loadView('table.formtable', compact('variables'));
+            });
+
+        })->download('xls');
+
+        return redirect()->back();
+    }
+
+    public function delete($id)
+    {
+        $variable = $this->variable->where('id', $id)->first();
+        $variable->delete();
+
+        return redirect()->back();
+    }
 }
